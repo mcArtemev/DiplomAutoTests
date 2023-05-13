bf285d9f4e95f15ce6678961f554c8e37527ac57
mcArtemev <mattartemev@gmail.com>
2023-05-14 01:51:17 +0300

    POM classed

---
+++ src/test/java/POM/RestorePasswordElements.java
@@ -0,0 +1,21 @@
+package POM;
+
+import POM.Helpers.Helpers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class RestorePasswordElements extends TestFixtures{
+    private By emailInput = By.name("name");
+    private By restoreButton = By.xpath("//button[contains(text(),'Восстановить')]");
+    private By loginLink = By.xpath("//a[contains(text(),'Войти')]");
+
+    public RestorePasswordElements(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+    public void clickLoginLink() {
+        driver.findElement(loginLink).click();
+    }
+
+}
